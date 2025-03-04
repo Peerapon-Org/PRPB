7333f7136d08ccf1dbc5b579ef72582a82b5b120
HeftyChonk <peerapon.boonkaweenapanon@gmail.com>
2025-03-05 00:03:30 +0700

    wip: homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-import mdx from "@astrojs/mdx";
-
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react(), mdx()],
+  integrations: [tailwind({ applyBaseStyles: false }), react()],
 });
