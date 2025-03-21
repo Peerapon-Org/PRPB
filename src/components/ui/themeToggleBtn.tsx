a9e578e846aa350abcfc244991a4f5bbcfbab24f
Peerapon <155229386+Peerapon966@users.noreply.github.com>
2025-03-21 04:36:07 +0000

    fix eslint failed code

--- src/components/ui/themeToggleBtn.tsx
+++ src/components/ui/themeToggleBtn.tsx
@@ -74,9 +74,9 @@ export function ThemeToggleBtn({ size = "md" }: ThemeToggleBtnProps) {
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className={styles.icon[size]}
         >
           <circle cx="12" cy="12" r="5"></circle>
@@ -97,9 +97,9 @@ export function ThemeToggleBtn({ size = "md" }: ThemeToggleBtnProps) {
           viewBox="0 0 24 24"
           fill="none"
           stroke="white"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           className={styles.icon[size]}
         >
           <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
