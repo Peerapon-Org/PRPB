7a1f3cb6f59d588993c94c9358bdb963cc61a166
HeftyChonk <peerapon.boonkaweenapanon@gmail.com>
2025-03-31 23:43:28 +0700

    wip: check changed file in test-blog workflow

--- .github/actions/check-changed-files/check.sh
+++ .github/actions/check-changed-files/check.sh
@@ -7,6 +7,9 @@ SLUG=${GITHUB_HEAD_REF#blog/}
 git diff --name-only $PREVIOUS_COMMIT origin/main > $TEMP
 cat $TEMP
 
+echo $($(cat $TEMP | wc -l) > 1)
+echo "$(cat $TEMP) != *src/pages/blog/$SLUG.md"
+
 if [[ $(cat $TEMP | wc -l) > 1 || $(cat $TEMP) != *"src/pages/blog/$SLUG.md" ]]; then
   echo "This PR expects only one blog post '$SLUG.md' to be modified"
   echo "If you want to modify multiple blog posts or other files, please create a new PR"
