d859766ba22f3d84a9a062bd09c20766a95f7da0
HeftyChonk <peerapon.boonkaweenapanon@gmail.com>
2025-04-02 22:22:17 +0700

    wip: check.sh

--- .github/actions/check-changed-files/check.sh
+++ .github/actions/check-changed-files/check.sh
@@ -9,5 +9,5 @@ git diff --name-only $PREVIOUS_COMMIT origin/main > $TEMP
 if [[ $(cat $TEMP | wc -l) > 1 || $(cat $TEMP) != *"src/pages/blog/$SLUG.md" ]]; then
   echo "This PR expects only one blog post 'src/pages/blog/$SLUG.md' to be modified"
   echo "If you want to modify multiple blog posts or other non-blog files, please create a new PR"
-  # exit 1
+  exit 1
 fi
