1f7aca6ee638519af1c579b35ed97117be3a0942
HeftyChonk <peerapon.boonkaweenapanon@gmail.com>
2025-04-12 00:37:01 +0700

    feat: fix github workflows

--- .github/actions/destroy-terraform/destroy.sh
+++ .github/actions/destroy-terraform/destroy.sh
@@ -2,6 +2,24 @@
 
 set -e
 
+BRANCH=$(awk -F' = ' '/^branch/ {print $NF}' $TFVARS_FILE | tr '\[/*\]' '-' | tr -d '"')
+IS_PRODUCTION=$(awk -F' = ' '/^is_production/ {print $NF}' $TFVARS_FILE | tr -d '"')
+
+export TF_VAR_account="$ACCOUNT_ID"
+export TF_VAR_hosted_zone_name=$DOMAIN_NAME
+
+[[ "$(aws apigateway get-account | jq -r '.cloudwatchRoleArn')" == null ]] && \
+  export TF_VAR_enable_account_logging="true" || \
+  export TF_VAR_enable_account_logging="false"
+
+if [[ -z $BRANCH ]]; then
+  export TF_VAR_branch=${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}
+fi
+
+if [[ "$IS_PRODUCTION" != "true" ]]; then
+  export TF_VAR_app_sub_domain_name=$WORKSPACE
+fi
+
 terraform destroy \
   -var-file "$TFVARS_FILE" \
   -auto-approve
