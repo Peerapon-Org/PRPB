0e682ac1ec9e9433196fd87df58341604a52c061
HeftyChonk <peerapon.boonkaweenapanon@gmail.com>
2025-04-16 02:50:37 +0700

    fix: publish-blog workflow

--- .github/actions/publish-blog/publish.sh
+++ .github/actions/publish-blog/publish.sh
@@ -4,13 +4,13 @@ set -e
 
 rollback () {
   echo "Failed publishing the blog, rolling back..."
-  aws s3 rm "s3://$(terraform output -raw s3_origin_bucket_name)/blog/$SLUG" --recursive
+  aws s3 rm "s3://$ORIGIN_BUCKET/blog/$SLUG" --recursive
   curl \
   -X DELETE \
   -H "Content-Type: application/json" \
   -H "X-API-Key: $API_KEY" \
   -d @items.json \
-  "$(terraform output -raw api_invoke_url)"/blogs
+  "$API_INVOKE_URL/blogs"
   exit 1
 }
 
@@ -23,11 +23,19 @@ TITLE=$(echo "$METADATA" | awk -F ': ' '/^title/ {print $NF}' | tr -d '"')
 DESCRIPTION=$(echo "$METADATA" | awk -F ': ' '/^description/ {print $NF}' | tr -d '"')
 THUMBNAIL=$(echo "$METADATA" | awk -F ': ' '/^thumbnail/ {print $NF}' | tr -d '"')
 
+ORIGIN_BUCKET=$(terraform output -raw s3_origin_bucket_name)
+API_INVOKE_URL=$(terraform output -raw api_invoke_url)
+DISTRIBUTION_ID=$(terraform output -raw distribution_id)
+APP_DOMAIN_NAME=$(terraform output -raw app_domain_name)
+
+echo $GITHUB_WORKSPACE
+
 trap rollback ERR
 
-aws s3 cp assets/blog/ "s3://$(terraform output -raw s3_origin_bucket_name)/blog/$SLUG/" --recursive
+aws s3 cp assets/blog/ "s3://$ORIGIN_BUCKET/blog/$SLUG/" --recursive
+pushd ../.github/actions/publish-blog > /dev/null 2>&1
 sed \
-  -i
+  -i \
   -e "s|<category>|$CATEGORY|g;" \
   -e "s|<subcategories>|${SUBCATEGORIES:1:-1}|g;" \
   -e "s|<date>|$DATE|g;" \
@@ -35,15 +43,15 @@ sed \
   -e "s|<title>|$TITLE|g;" \
   -e "s|<description>|$DESCRIPTION|g;" \
   -e "s|<thumbnail>|$THUMBNAIL|g;" \
-  ../.github/actions/publish-blog/items.json
+  items.json
 curl \
   -X POST \
   -H "Content-Type: application/json" \
   -H "X-API-Key: $API_KEY" \
   -d @items.json \
-  "$(terraform output -raw api_invoke_url)"/blogs
+  "$API_INVOKE_URL/blogs"
 
-aws cloudfront create-invalidation --distribution-id "$(terraform output -raw distribution_id)" --paths "/api/*"
+aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION_ID" --paths "/api/*"
 
-echo "blog-url=$(terraform output -raw app_domain_name)/blog/$SLUG" >> "$GITHUB_OUTPUT"
+echo "blog-url=$APP_DOMAIN_NAME/blog/$SLUG" >> "$GITHUB_OUTPUT"
 echo "Blog publish completed!"
