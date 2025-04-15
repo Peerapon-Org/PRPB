73f32bf12fb377838756d78a842ec8bb8204c47d
Peerapon <155229386+Peerapon966@users.noreply.github.com>
2025-04-16 03:38:06 +0700

    Blog/s3 pre signed url vs cloudfront signed url (#99)
    * fix: publish-blog workflow
    
    * fix: publish-blog workflow
    
    * fix: publish-blog workflow
    
    * fix: publish-blog workflow
    
    * fix: publish-blog workflow
    
    * fix: publish-blog workflow

--- .github/actions/publish-blog/publish.sh
+++ .github/actions/publish-blog/publish.sh
@@ -17,7 +17,7 @@ rollback () {
 SLUG=${GITHUB_HEAD_REF#blog/}
 METADATA=$(sed 10q "../src/pages/blog/$SLUG.mdx")
 CATEGORY=$(echo "$METADATA" | awk -F ': ' '/^category/ {print $NF}' | tr -d '"')
-SUBCATEGORIES=$(echo "$METADATA" | awk -F ': ' '/^subcategories/ {print $NF}' | jq -Rr @json)
+SUBCATEGORIES=$(echo "$METADATA" | awk -F ': ' '/^subcategories/ {print $NF}')
 DATE=$(echo "$METADATA" | awk -F ': ' '/^date/ {print $NF}' | tr -d '"')
 TITLE=$(echo "$METADATA" | awk -F ': ' '/^title/ {print $NF}' | tr -d '"')
 DESCRIPTION=$(echo "$METADATA" | awk -F ': ' '/^description/ {print $NF}' | tr -d '"')
@@ -28,22 +28,24 @@ API_INVOKE_URL=$(terraform output -raw api_invoke_url)
 DISTRIBUTION_ID=$(terraform output -raw distribution_id)
 APP_DOMAIN_NAME=$(terraform output -raw app_domain_name)
 
-echo $GITHUB_WORKSPACE
-
 trap rollback ERR
 
 aws s3 cp assets/blog/ "s3://$ORIGIN_BUCKET/blog/$SLUG/" --recursive
 pushd ../.github/actions/publish-blog > /dev/null 2>&1
-sed \
-  -i \
-  -e "s|<category>|$CATEGORY|g;" \
-  -e "s|<subcategories>|${SUBCATEGORIES:1:-1}|g;" \
-  -e "s|<date>|$DATE|g;" \
-  -e "s|<slug>|$SLUG|g;" \
-  -e "s|<title>|$TITLE|g;" \
-  -e "s|<description>|$DESCRIPTION|g;" \
-  -e "s|<thumbnail>|$THUMBNAIL|g;" \
-  items.json
+jq \
+  --arg title "$TITLE" \
+  --arg description "$DESCRIPTION" \
+  --arg thumbnail "$THUMBNAIL" \
+  --arg slug "$SLUG" \
+  --arg category "$CATEGORY" \
+  --argjson subcategories "$SUBCATEGORIES" \
+  --arg date "$DATE" \
+  '.blogs = [{"title": $title, "description": $description, "thumbnail": $thumbnail, "slug": $slug, "category": $category, "subcategories": $subcategories, "publishDate": $date}]' \
+  .github/actions/publish-blog/items.json > items.json.tmp
+mv items.json.tmp items.json
+
+cat items.json
+
 curl \
   -X POST \
   -H "Content-Type: application/json" \
